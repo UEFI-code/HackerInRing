77303c9158815ecd3bf5cb1d8cbd2dedbeed7362
UEFI-code <zhuoshizhang@hotmail.com>
2024-03-07 22:24:21 +0800

    Refactor myVM class and add new instructions

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
 from typing import Any
 
-
-ins_Dict = {
-    'sayhello': [0,'print("Hello, World!")'],
-    'nop': [0, 'pass'],
-    'sleep': [0, 'import time\ntime.sleep(1)'],
-    'jmp': [1],
-    'writemem': [1],
-    'readmem': [1],
-    'ret': [2],
-    'sysenter': [3],
-    'call': [4],
-}
-
 class myVM:
     def __init__(self, mem):
         self.ins_Dict = {
             'sayhello': [0,'print("Hello, World!")'],
+            'printpc': [0, 'print(self.pc)'],
+            'printretaddr': [0, 'print(self.ret_addr)'],
+            'printmemsize': [0, 'print(len(self.mem))'],
             'nop': [0, 'pass'],
             'sleep': [0, 'import time\ntime.sleep(1)'],
             'jmp': [1],
-            'writemem': [1],
-            'readmem': [1],
-            'ret': [2],
-            'sysenter': [3],
-            'call': [4],
-            'getmember': [5],
-            'kill': [6],
-            'humanloop': [7],
+            'writemem': [2],
+            'readmem': [3],
+            'ret': [4],
+            'sysenter': [5],
+            'call': [6],
+            'getmember': [7],
+            'kill': [8],
+            'humanloop': [9],
         }
         self.mem = mem
+        self.ret_addr = 0
+        self.syscall_table = [0] * 128
         self.pc = 0
         self.sp = 0
         self.bp = 0
@@ -39,43 +31,106 @@ class myVM:
         self.run_cmd_humanloop()
 
     def run_cmd(self):
-        while self.pc < len(self.mem):
-            preprocess_cmd = self.mem[self.pc].split(' ')
-            if preprocess_cmd[0] in ins_Dict:
-                print(f'PC: {self.pc}, Running command: {preprocess_cmd}')
-                cmd_type = ins_Dict[preprocess_cmd[0]][0]
-                if cmd_type == 0:
-                    exec(ins_Dict[preprocess_cmd[0]][1])
-                self.pc += 1
-            else:
-                print('Unknown command: ' + preprocess_cmd[0])
-        print('Code Pointer out of range!')
-        self.crash_handler()
+        try:
+            while self.pc < len(self.mem):
+                preprocess_cmd = self.mem[self.pc].split(' ')
+                if preprocess_cmd[0] in self.ins_Dict:
+                    print(f'PC: {self.pc}, Running command: {preprocess_cmd}')
+                    cmd_type = self.ins_Dict[preprocess_cmd[0]][0]
+                    if cmd_type == 0:
+                        exec(self.ins_Dict[preprocess_cmd[0]][1])
+                    elif cmd_type == 1:
+                        self.pc = int(preprocess_cmd[1])
+                        if self.pc < 0 or self.pc >= len(self.mem):
+                            print('Code Pointer out of range!')
+                            self.crash_handler()
+                        print('Jumping to ' + str(self.pc))
+                        continue
+                    elif cmd_type == 2:
+                        addr = int(preprocess_cmd[1])
+                        val = int(preprocess_cmd[2])
+                        if addr < 0 or addr >= len(self.mem):
+                            print('Memory address out of range!')
+                            self.crash_handler()
+                        self.mem[addr] = val
+                        print('Writing ' + str(val) + ' to ' + str(addr))
+                    elif cmd_type == 3:
+                        addr = int(preprocess_cmd[1])
+                        if addr < 0 or addr >= len(self.mem):
+                            print('Memory address out of range!')
+                            self.crash_handler()
+                        print(f'Memory value at {addr}: {self.mem[addr]}')
+                    elif cmd_type == 4:
+                        self.pc = self.ret_addr
+                        if self.pc < 0 or self.pc >= len(self.mem):
+                            print('Code Pointer out of range!')
+                            self.crash_handler()
+                        print('Returning to ' + str(self.pc))
+                        continue
+                    self.pc += 1
+                else:
+                    print('Unknown command: ' + preprocess_cmd[0])
+            print('Code Pointer out of range!')
+            self.crash_handler()
+        except Exception as e:
+            print('Error: ' + str(e))
+            self.crash_handler()
         
     def run_cmd_humanloop(self):
-        while True:
-            cmd = input('Enter command: ')
-            preprocessed_cmd = cmd.split(' ')
-            if preprocessed_cmd[0] in ins_Dict:
-                print(f'Running command: {preprocessed_cmd}')
-                cmd_type = ins_Dict[preprocessed_cmd[0]][0]
-                if cmd_type == 0:
-                    exec(ins_Dict[preprocessed_cmd[0]][1])
-                elif cmd_type == 1:
-                    self.pc = int(preprocessed_cmd[1])
-                    if self.pc < 0 or self.pc >= len(self.mem):
-                        print('Invalid address')
-                        self.pc = 0
-                        continue
-                    sure = input(f'Are you sure you want to jump to {self.pc}? (y/n): ')
-                    if sure == 'n':
-                        self.pc = 0
-                        print('You are so cowardly!!!')
-                        continue
-                    print('Jumping to ' + str(self.pc))
-                    self.run_cmd()
-            else:
-                print('Unknown command: ' + preprocessed_cmd[0])
+        try:
+            while True:
+                cmd = input('Enter command: ')
+                preprocessed_cmd = cmd.split(' ')
+                if preprocessed_cmd[0] in self.ins_Dict:
+                    print(f'Running command: {preprocessed_cmd}')
+                    cmd_type = self.ins_Dict[preprocessed_cmd[0]][0]
+                    if cmd_type == 0:
+                        exec(self.ins_Dict[preprocessed_cmd[0]][1])
+                    elif cmd_type == 1:
+                        self.pc = int(preprocessed_cmd[1])
+                        if self.pc < 0 or self.pc >= len(self.mem):
+                            print('Invalid address')
+                            self.pc = 0
+                            continue
+                        sure = input(f'Are you sure you want to jump to {self.pc}? (y/n): ')
+                        if sure == 'n':
+                            self.pc = 0
+                            print('You are so cowardly!!!')
+                            continue
+                        print('Jumping to ' + str(self.pc))
+                        self.run_cmd()
+                    elif cmd_type == 2:
+                        addr = int(preprocessed_cmd[1])
+                        val = int(preprocessed_cmd[2])
+                        if addr < 0 or addr >= len(self.mem):
+                            print('Invalid address')
+                            continue
+                        self.mem[addr] = val
+                        print('Writing ' + str(val) + ' to ' + str(addr))
+                    elif cmd_type == 3:
+                        addr = int(preprocessed_cmd[1])
+                        if addr < 0 or addr >= len(self.mem):
+                            print('Invalid address')
+                            continue
+                        print(f'Memory value at {addr}: {self.mem[addr]}')
+                    elif cmd_type == 4:
+                        self.pc = self.ret_addr
+                        if self.pc < 0 or self.pc >= len(self.mem):
+                            print('Invalid address')
+                            self.pc = 0
+                            continue
+                        sure = input(f'Are you sure you want to return to {self.pc}? (y/n): ')
+                        if sure == 'n':
+                            self.pc = 0
+                            print('You are so cowardly!!!')
+                            continue
+                        print('Returning to ' + str(self.pc))
+                        self.run_cmd()
+                else:
+                    print('Unknown command: ' + preprocessed_cmd[0])
+        except Exception as e:
+            print('Error: ' + str(e))
+            self.crash_handler()
 
 if __name__ == '__main__':
     mem = ['nop'] * 128
